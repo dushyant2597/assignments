17f12fc4f019329c760b3062a1a18d43769cdb0c
dushyant2597 <dushyantsharma250797@gmail.com>
2018-09-28 14:42:06 +0530

    first commit

---
+++ dushyant.py
@@ -0,0 +1,11 @@
+Python 2.7.15 (v2.7.15:ca079a3ea3, Apr 30 2018, 16:30:26) [MSC v.1500 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> hello
+
+Traceback (most recent call last):
+  File "<pyshell#0>", line 1, in <module>
+    hello
+NameError: name 'hello' is not defined
+>>> print("hello")
+hello
+>>> 
